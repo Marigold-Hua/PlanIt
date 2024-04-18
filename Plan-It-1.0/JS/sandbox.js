f9e9cb8a48e6ff40e141dab7906ce98e2d4baf51
Mari <marilyn3@andrew.cmu.edu>
2024-04-18 16:50:17 -0400

    ?

--- Plan-It-1.0/JS/sandbox.js
+++ Plan-It-1.0/JS/sandbox.js
@@ -1,24 +1,84 @@
 let startTime = 9;
 let endTime = 21;
 
+const dayArray = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
 document.addEventListener('DOMContentLoaded', makeBoxes());
 
+/*
 function makeBoxes(){
+
+    //Generate days
+    const weekContainer = document.querySelector('.weekContainer');
+
+    const dayContainer = document.createElement('div');
+        dayContainer.className = "day";
+
+        weekContainer.append(dayContainer);
+
+
+    for(day in dayArray){
+        const dayName = dayArray[day];
+        
+        //Create container for day
+        const dayContainer = document.createElement('div');
+        dayContainer.className = "day";
+
+        weekContainer.append(dayContainer);
+
+
+        //Create div for day header
+        const dayHeader = document.createElement("div");
+
+    }
+
+    //Generate hours in day
+
+    //Generate quarters in day
+}
+*/
+
+function makeBoxes(){
+
     // Get the container element
     const container = document.querySelector('.weekContainer');
 
-    //Generate 4 Boxes
-    for (h = startTime; h < endTime+1; h++){
-        const newHour = document.createElement("div");
-        container.append(newHour);
-        newHour.className = "hour";
-        newHour.id = h;
-        for (q = 0; q < 4; q++){
-            const hourContainer = document.getElementById(h);
-            const newBox = document.createElement("div");
-            newBox.className = "quarter";
+    //Generate calendar
+    
+    //Generate days
+    for (day in dayArray){
         
-            hourContainer.appendChild(newBox);
+        //Create current day
+        const newDay = document.createElement('div');
+        newDay.className = 'day';
+        newDay.id = day;
+
+        //Create container for hours in day
+        const hourContainer = document.createElement('div');
+        
+        container.append(newDay);
+
+        //Generate hours
+        for (h = startTime; h < endTime+1; h++){
+
+            //Create current hour
+            const newHour = document.createElement('div');
+            newHour.className = 'hour';
+            newHour.id = h;
+            container.append(newHour);
+
+            //Generate quarters
+            for (q = 0; q < 4; q++){
+
+                //Create current quarter
+                const hourContainer = document.getElementById(h);
+                const newBox = document.createElement('div');
+                newBox.className = 'quarter';
+
+                //Append quarter to hour container
+                hourContainer.appendChild(newBox);
+            }
         }
     }
   };
+  
