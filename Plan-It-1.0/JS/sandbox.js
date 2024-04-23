851a412eef3f36344ad2fe10e743da99c1b34821
Mari <marilyn3@andrew.cmu.edu>
2024-04-22 21:37:12 -0400

    Link clicks on project to function, update formatting, draft interaction with quarters

--- Plan-It-1.0/JS/sandbox.js
+++ Plan-It-1.0/JS/sandbox.js
@@ -19,10 +19,11 @@ APIs and Libraries to Check Out
 let startTime = 17;
 let endTime = 21;
 let projectNum = 0;
-let currentProject = "None";
+let projectSelected; //so that whenever projectNum is updated, project select is out of range. 
 let projectList;
 
-const dayArray = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+//const dayArray = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+const dayArray = ['S', 'M', 'T', 'W', 'T', 'F', 'S'];
 const colorArray = ['#76da71', '#71daac','#71cfda', '#719bda', '#7371da', '#c071da'];
 const projectNames = ['Exercise', 'PUI Project', 'Webtoon', 'Meals', 'Social', 'Research'];
 
@@ -31,6 +32,38 @@ document.addEventListener('DOMContentLoaded', makeCalendar);
 
 //Call function to add project when project button is clicked
 document.querySelector('#add-project-button').addEventListener('click', addProject);
+
+/* In-progress: Tests for triggering addTask on click within calendar times
+//Use event delegation to pass clicks from each nested element of the week (from week container to quarter)
+//Select all elemnts with a corresponding class
+const allWeek = document.querySelector('.week-container');
+const allDay = document.querySelectorAll('.day');
+const allHour = document.querySelectorAll('.hour');
+const allQuarter = document.querySelectorAll('.quarter');
+
+//Add event listner to week
+allWeek.addEventListener('click', test)
+//Add event 
+/* //Add event listner to each quarter
+console.log
+quarters.forEach(quarter => {
+quarter.addEventListener('click', addTask); 
+});
+*/
+//Get all elements with quarter class
+const allQuarters = document.querySelectorAll('.quarter');
+
+//Add event listner to each quarter
+allQuarters.forEach(quarter => {
+quarter.addEventListener('click', test); 
+});
+
+//test event delegaton
+function test(event){
+    event.stopPropagation();
+    console.log('working');
+}
+
 
 //Function calls all functions used to make calendar
 function makeCalendar(){
@@ -45,7 +78,7 @@ function makeTimeKey(){
 
     //Add header to time key 
     const timeKeyHeader = document.createElement('div');
-    timeKeyHeader.className = 'calHeader';
+    timeKeyHeader.className = 'cal-header';
     timeKeyHeader.innerText = 'Time';
     timeKey.append(timeKeyHeader);
 
@@ -142,6 +175,11 @@ function makeWeek(){
         project.innerText = projectNames[projectNum];
         project.style.color = colorArray[projectNum % (colorArray.length)];
         project.style.border = colorArray[projectNum % (colorArray.length)] + " solid 3px";
+
+        //Call function that clears the project selected
+        clearProjectSelected();
+
+        // Update number of projects
         projectNum++;
 
         //Add cloned project to DOM
@@ -149,7 +187,7 @@ function makeWeek(){
         projectManager.append(project);
 
         //Add event listner to new project - work in progress -
-        document.querySelector('#'+'project.id').addEventListener('click', updateProjectSelected);
+        document.querySelector('#'+project.id).addEventListener('click', updateProjectSelected);
         /*
         //Update event listners and project list
         projectList = document.querySelectorAll('.project');
@@ -166,7 +204,22 @@ function makeWeek(){
     console.log("Project limit reached");
   }
 
+  //Clears projectSelected (+1 to projectNum, making it out of bounds of later logic)
+  function clearProjectSelected(){
+    projectSelected = projectNum + 1;
+  }
+
   //Keeps track of which project is currently selected  - work in progress -
-  function updateProjectSelected(){
-    console.log();
+  function updateProjectSelected(event){
+
+    /* Test to get project id number from id
+    currentProject = project.id;
+    const projectInfo = projectID.split('-');
+
+    console.log(projectInfo[1]);
+    */
+
+    //Connects to id of project that triggered the event
+    var projectInfo = event.target.id.split('-');
+    projectSelected = projectInfo;
   }
