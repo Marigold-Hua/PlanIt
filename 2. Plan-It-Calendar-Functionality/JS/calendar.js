dd5287e985e0eefc7801a88eb9562da4559aed71
Mari <marilyn3@andrew.cmu.edu>
2024-04-27 09:59:09 -0400

    Generate Gradient

--- 2. Plan-It-Calendar-Functionality/JS/calendar.js
+++ 2. Plan-It-Calendar-Functionality/JS/calendar.js
@@ -7,8 +7,8 @@
 
 //Variables that will be set by user
 //Note that time variables are taken in as 24 hour time but displayed as 12 hour time
-let startTime = 9;  //start of "most productive hours"
-let endTime = 15; //End of "most productive hours"
+let startTime = 6;  //start of "most productive hours"
+let endTime = 11; //End of "most productive hours"
 //note that (endTime - startTime + 1)*4 -1 is the max quarter number for each day
 let taskLimit = 5; //max amount of tasks user can add
 
@@ -19,8 +19,9 @@ let isTaskSelected = false;
 
 //Color Variables
 const backgroundColor = window.getComputedStyle(document.body).backgroundColor; //get background color
-console.log(backgroundColor);
-const colorArray = ['rgb(118, 218, 113)', 'rgb(113, 207, 218)']; //simplified colorArray
+const colorArray = ['rgb(118, 218, 113, 1)', 'rgb(113, 207, 218, 1)']; //simplified colorArray
+const colorRGBArray = [118, 218, 113, 113, 207, 218]; 
+const color1and2Mix = ['rgb(118, 218, 113, 1)', 'rgb(117,216,134)', 'rgb(116,214,155)', 'rgb(115,211,176)', 'rgb(114,209,197)', 'rgb(113, 207, 218, 1)']
 //Arrays of information. 
 const dayArray = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 //const dayArray = ['S', 'M', 'T', 'W', 'T', 'F', 'S']; //Optional version of dayArray using single letters for day names
@@ -28,7 +29,7 @@ const dayArray = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Frida
 //const colorArray = ['rgb(118, 218, 113)', 'rgb(113, 218, 172)','rgb(113, 207, 218)', 'rgb(113, 155, 218)', 'rgb(115, 113, 218)', 'rgb(192, 113, 218)'];
 //const taskNames = ['Make Bed', 'Do Laundry', 'Sweep floor', 'Do Dishes', 'Shower', 'Brush Hair'];
 
-const taskNames = ["Work on Pui", "Plan Social Event"]; //simplified list
+const taskNames = ['Work on Pui', 'Plan Social Event']; //simplified list
 /*Note that the ID of the quarter is what many functiond depend on. Quarter IDs are arranged as follows
     Day-Hour-MacroQuarter-NumberOfTasksInBlock-T1-T2-T3-G1-G2-G3
 
@@ -36,19 +37,34 @@ const taskNames = ["Work on Pui", "Plan Social Event"]; //simplified list
         - [0]:Day - what day of the week the quarter is in
         - [1]: Hour - what hour of the day the quarter is in (might be calculatable with floor?)
         - [2]: Macro quarter - what quarter of the day the quarter is
-        - [3]: Number of Tasks in Block - A number between 0 and 3 representing how many tasks overlap with that quarter //may remove depending on use of T ID portions
-        - [4, 5]: T1 - T2, the task nunmbers corresponding to the tasks overlapping the quarter. Set to tasktNames.length if none. 
 */
 
 /*Code involved in generating calendar*/
 
-//On website load, this line calls the function that generates the calendar. 
-document.addEventListener('DOMContentLoaded', makeCalendar);
+//On website load, this line calls the function that checks the dimensions of the webpage. 
+document.addEventListener('DOMContentLoaded', checkDimensions);
 
-//Function calls all functions involved in dynamically generating calendar
+//On website resize, this line calls the function that checks the dimensions of the webpage. 
+document.addEventListener('resize', checkDimensions);
+
+//Function checks if the page is wide screen or narrow and calls the corrsponding functions to create differently formatted calendar
+function checkDimensions(){
+    if (screen.width > 1000){
+        makeCalendar();
+    } else {
+        makeCalendarCarosuel();
+       }
+}
+
+//Function calls all functions involved in dynamically generating calendar for wider screen
 function makeCalendar(){
     makeTimeKey();
-    makeWeek();
+    makeWeek(true);
+}
+
+//Function calls all functions involved in dynamically generating calendar for smaller
+function makeCalendarCarosuel(){
+   makeWeek(false); 
 }
 
 //Makes time key, the element to the left of the week that labels the dynamically generated hours
@@ -82,63 +98,15 @@ function makeTimeKey(){
  }
 
  //Generates week 
-function makeWeek(){
+function makeWeek(hasTime){
 
     // Get the week container element
-    const container = document.querySelector('.week-container');
+    const container = document.querySelector('.calendar-container');
 
     //Generate day within week container
     for (day in dayArray){
 
-        //Create new day
-        const newDay = document.createElement('div');
-        newDay.className = 'day';
-        newDay.id = dayArray[day];
-        container.append(newDay);
-
-        //Create heading of day
-        const dayHeader = document.createElement('div');
-        dayHeader.className = 'day-header';
-        dayHeader.innerText = dayArray[day];
-        newDay.append(dayHeader);
-
-        //Generate time for day, starting with hours
-        //Create container for hours within day
-        const hourContainer = document.createElement('div');
-        hourContainer.className = 'hour-container';
-        hourContainer.id = dayArray[day] + '-hour-container';
-
-        //Add hour container to day 
-        newDay.append(hourContainer);
-
-       macroQuarterID  = 0; //the quarter number within the entire day
-
-        //Generate hours
-        for (h = startTime; h <= endTime; h++){
-
-            //Create current hour
-            const newHour = document.createElement('div');
-            newHour.className = 'hour';
-            newHour.id = dayArray[day] + '-' + h;
-
-            //Add current hour to hour container
-            hourContainer.append(newHour);
-
-            //Generate quarters
-            for (q = 0; q < 4; q++){
-
-                //Create current quarter
-                const quarterContainer = document.createElement('div');
-                quarterContainer.className = 'quarter'; //should assign q based on total q - i.e. the 3nd quarter in the 2nd hour would have q value of (2-1)*4+3
-                quarterContainer.id = dayArray[day] + '-' + h + '-' + macroQuarterID + "-" 
-                    + 0 //number of tasks within quarter
-                    + "-" + taskNames.length + "-" + taskNames.length;
-
-                //Append quarter to hour container
-                newHour.appendChild(quarterContainer);
-                macroQuarterID++;
-            }
-        }
+        makeDay(day, hasTime);
 
     }
 
@@ -148,9 +116,59 @@ function makeWeek(){
      allQuarters.forEach(quarter => {
        quarter.addEventListener('click', makeGradient)
      })
+};
 
-  };
+  //makes each day 
+function makeDay(day, hasTime){
 
+    //Create new day
+    const newDay = document.createElement('div');
+    newDay.className = 'day';
+    newDay.id = dayArray[day];
+    document.querySelector('.calendar-container').append(newDay);
+
+    //Create heading of day
+    const dayHeader = document.createElement('div');
+    dayHeader.className = 'day-header';
+    dayHeader.innerText = dayArray[day];
+    newDay.append(dayHeader);
+
+    //Generate time for day, starting with hours
+    //Create container for hours within day
+    const quarterContainer = document.createElement('div');
+    quarterContainer.className = 'quarter-container';
+    quarterContainer.id = dayArray[day] + '-quarter-container';
+
+    //Add hour container to day 
+    newDay.append(quarterContainer);
+
+    macroQuarterID  = 0; //the quarter number within the entire day
+
+    //Generate 4 quarters for each hour
+    for (h = startTime; h <= endTime; h++){
+
+        //Generate quarters
+        for (q = 0; q < 4; q++){
+
+            //Create current quarter
+            const quarter = document.createElement('div');
+            quarter.className = 'quarter'; //should assign q based on total q - i.e. the 3nd quarter in the 2nd hour would have q value of (2-1)*4+3
+            quarter.id = dayArray[day] + '-' + h + '-' + macroQuarterID;
+
+            //Append quarter to hour container
+            if (q % 4 === 0 && !hasTime){
+                if (h <= 12) {
+                    quarter.innerText = h + ':00 AM';
+                }
+                else {
+                    quarter.innerText = h-12 + ':00 PM';
+                }
+            }
+            quarterContainer.appendChild(quarter);
+            macroQuarterID++;
+        }
+    }
+}
 
 /* Code involved in managing tasks */
 
@@ -221,7 +239,7 @@ function addTask(){
             task.style.color = 'black';
         }
        else if (task.classList.contains('selected')){
-            task.style.border = task.style.backgroundColor + " solid 3px";
+            task.style.border = task.style.backgroundColor + ' solid 3px';
             task.style.color = task.style.backgroundColor;
             task.style.backgroundColor = 'black';
             task.classList.remove('.selected');
@@ -230,83 +248,30 @@ function addTask(){
 
     //Sets task selected equal to the task number associated with the clicked task
     whichTaskNumSelected = taskInfo[1];
-
-    console.log(taskSelected + '' + isTaskSelected); //test if correct taskNumber is being set
   }
 
   //Makes gradients based on what and how many tasks are nearby, and what task is currently selected
   function makeGradient(event){
-
-    const clickedQuarterID = event.target.id;
+    
+    event.target.style.backgroundColor = colorArray[whichTaskNumSelected];
     const fullQuarterIDName = event.target.id.split('-');
-    //whichTaskNumSelected
     const day = fullQuarterIDName[0];
     const hour = fullQuarterIDName[1];
-    const macroQuarterID = fullQuarterIDName[2];
-    let task1ID = fullQuarterIDName[4];
-    let task2ID = fullQuarterIDName[5];
-    const quarterDOMElement = document.getElementById(clickedQuarterID);
+    const macroQuarter = parseInt(fullQuarterIDName[2]);
 
-    quarterDOMElement.style.backgroundColor = colorArray[whichTaskNumSelected];
-
-    /* Gradient Trial 1 (fix later)
-
-    //changes color of selected quarter to that corresponding to color of the task
-    if(quarterDOMElement.classList.contains('gradient0') || quarterDOMElement.classList.contains('gradient1') ) //makes sure added task is atleast 1 quarters/15 minutes away from core of next task
-    {}
-
-    } else 
-    {
-        quarterDOMElement.style.backgroundColor = colorArray[whichTaskNumSelected];
-
-        //IDs of quarters n (1-2_ above (a) and below (b)
-        const firstNumAbove = macroQuarterID-1:
-        const secondNumAbove = firstNumAbove-1;
-        const firstNumBelow = macroQuarterID+1:
-        const secondNumBelow = firstNumBelow+1;
-        const target2A = day + "-" + hour + "-" + firstNumAbove + ""
-        const target1A
-        const target1B
-        const target2B
-    }
-    */
-
-    
-    /* correct logic later
-    //Logic checks to see if task spaces have been filled by another task
-    if (task1ID = taskNames.length){
-        setGradient1(clickedQuarterID);
-    }
-        else if (task2ID = taskNames.length) {
-
+     //Define gradient values
+     const gradientAlpha = [1, 0.5, 0.25];
+     
+    //Seperate get the R, G, B values and append the A value
+   
+     console.log(event.target.id);
+     //Gets each quarter within 2 neighbors of the clicked quarter
+     for (let relQuarter = -2; relQuarter < 3; relQuarter++){
+        const quarterNeighbor = macroQuarter + parseInt(relQuarter);
+        const quarterNeighborID = day + '-' + hour + '-' + quarterNeighbor;
+        const quarterNeighborDOM = document.getElementById(quarterNeighborID)
+        if(quarterNeighborDOM){
+            quarterNeighborDOM.style.backgroundColor = 'rgba(' + colorRGBArray[3*whichTaskNumSelected] + ',' + colorRGBArray[3*whichTaskNumSelected+1] + ',' + colorRGBArray[3*whichTaskNumSelected+2] + ',' + gradientAlpha[Math.abs(relQuarter)] + ')';
         }
-            else if (task3ID = taskNames.length) {
-
-            }
-                else {
-                    atLimit();
-                }
-
-    //console.log(macroQuarterID + "-" + whichTaskNumSelected); //test
-                */
-    
-    /*if (taskSelected) {
-      event.target.style.backgroundColor = colorArray[taskNum];
-      event.
-    }*/
-
-    //console.log(quarterHour + ' ' + quarterID + ' ' + quarterMacroId); //test 
-  }
-
- 
-  function setGradient1(clickedQuarterID){
-    //Get quarters impacted by change, on above quarter clicked
-    //Check if quarters above are within quarter bounds
-    document.getElementById('#' + clickedQuarterID).backgroundColor = colorArray[whichTaskNumSelected];
-    if (macroQuarterID-1 >= 0){ //Check if quarter directly above is within bounds
-        //if (macroQuarterID-2 >= 0){ //Check if quarter two rows above is within bounds
     }
-    //Get quarters impacted by change, on below quarter clicked
-    //Check if quarters below are within quarter bonds
   }
-  
